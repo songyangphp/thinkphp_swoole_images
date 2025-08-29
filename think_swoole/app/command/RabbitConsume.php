266656c14d51ebe9fa3c5aae38373253780d8a1c
songyangphp <475511896@qq.com>
2025-08-29 14:55:05 +0800

    支持rabbitmq拓展

--- think_swoole/app/command/RabbitConsume.php
+++ think_swoole/app/command/RabbitConsume.php
@@ -3,6 +3,7 @@ declare (strict_types = 1);
 
 namespace app\command;
 
+use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use think\console\Command;
 use think\console\Input;
@@ -15,48 +16,108 @@ use think\console\Output;
  */
 class RabbitConsume extends Command
 {
+    /**
+     * 配置指令
+     * @return void
+     */
     protected function configure()
     {
         // 指令配置
-        $this->setName('rabbitConsume')->setDescription('rabbitmq 模拟消费者');
+        $this->setName('rabbitConsume')
+            ->addOption('type','t',Option::VALUE_REQUIRED,'请选择类型1直连 2主题',1)
+            ->setDescription('rabbitmq 模拟消费者');
     }
 
+    /**
+     * 执行指令
+     * @param Input $input
+     * @param Output $output
+     * @return mixed
+     * @throws \Exception
+     */
     protected function execute(Input $input, Output $output)
     {
-
+        $type = intval($input->getOption('type'));
+        $typeArray = [
+            1 => 'direct', // 直连模式
+            2 => 'topic' // 主题模式-发布&订阅
+        ];
+        if(!isset($typeArray[$type])){
+            return 'type fail';
+        }
 
         // 创建连接到 RabbitMQ 服务器
         $connection = new AMQPStreamConnection('host.docker.internal', 5672, 'admin', 'admin321');
         $channel = $connection->channel();
 
-        // 声明队列
-
-        echo " [*] Waiting for messages. To exit press CTRL+C\n";
-
-        // 回调函数，用于处理接收到的消息
-        $callback = function ($msg) {
-            echo ' [x] Received ', $msg->body, "\n";
-            // 模拟任务处理
-            sleep(3);
-            echo " [x] Done\n";
-            // 手动确认消息已经处理
-            $msg->ack();
-        };
-
-        // 告诉 RabbitMQ 在同一时间不要发送多于一条消息给一个消费者
-        $channel->basic_qos(0, 1, null);
-
-        // 告诉 RabbitMQ 使用回调函数来接收消息，并手动确认消息
-        $channel->basic_consume('task_queue', '', false, false, false, false, $callback);
-
-        // 等待消息进入队列
-        while ($channel->is_consuming()) {
-            $channel->wait();
-        }
+        $function = $typeArray[$type]."type";
+        $this->$function($channel);
 
         // 关闭连接
         $channel->close();
         $connection->close();
+    }
 
+    /**
+     * 直连模式
+     * @param $channel
+     * @return void
+     */
+    private function directType(AMQPChannel $channel)
+    {
+        $exchangeName = 'test_exchange'; // exchange的名称
+
+        // 声明 Exchange
+        $channel->exchange_declare($exchangeName, 'direct', false, true, false);
+
+        // 声明一个队列 让服务器为我们选择一个随机名称
+        list($queueName,,) = $channel->queue_declare('', false, false, true,false);
+
+        // 将队列绑定到exchange 并指定binding Key
+        $bindKey = 'test_routing_key';
+        $channel->queue_bind($queueName, $exchangeName, $bindKey);
+
+        echo " [*] Waiting for messages on queue ’" . $queueName . "’. To exit press CTRL+C\n";
+
+        // 定义消息处理回调函数
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+        };
+
+        // 开始消费队列中的消息
+        $channel->basic_consume($queueName, '', false, true, false, false, $callback);
+        while ($channel->is_consuming()) {
+            $channel->wait();
+        }
+    }
+
+
+    /**
+     * 主题模式-发布&订阅
+     * @param $channel
+     * @return void
+     */
+    private function topicType(AMQPChannel $channel)
+    {
+        $exchangeName = 'topic_exchange'; // exchange的名称
+
+        // 声明队列（让服务器生成随机队列名）
+        list($queueName, ,) = $channel->queue_declare("", false, false, true, false);
+
+        // 绑定路由键：接收所有天气预报相关的消息（只需要改bindKey(routingKey)，就可以实现接收不同bindKey的消息）
+        $bindingKey = '#.weather';
+        $channel->queue_bind($queueName, $exchangeName, $bindingKey);
+
+        echo " [*] Consumer 1 waiting for messages with binding key '" . $bindingKey . "'. To exit press CTRL+C\n";
+
+        $callback = function ($msg) {
+            echo " [x] Consumer 1 Received '" . $msg->body . "' with routing key '" . $msg->get('routing_key') . "'\n";
+        };
+
+        $channel->basic_consume($queueName, '', false, true, false, false, $callback);
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+        }
     }
 }
--- think_swoole/app/controller/Rabbitmq.php
+++ think_swoole/app/controller/Rabbitmq.php
@@ -3,9 +3,9 @@
 namespace app\controller;
 
 use app\BaseController;
+use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
-
 
 /**
  * rabbitmq 消息中间件
@@ -14,31 +14,89 @@ class Rabbitmq extends BaseController
 {
     /**
      * 模拟生产者
-     * @return void
+     * @return mixed
      * @throws \Exception
      */
     public function producer()
     {
+        $type = $this->request->get('type',1,'intval');
+        $typeArray = [
+            1 => 'direct', // 直连模式
+            2 => 'topic' // 主题模式-发布&订阅
+        ];
+        if(!isset($typeArray[$type])){
+            return 'type fail';
+        }
 
         // 创建连接到 RabbitMQ 服务器
         $connection = new AMQPStreamConnection('host.docker.internal', 5672, 'admin', 'admin321');
         $channel = $connection->channel();
 
-        // 声明队列
-        $channel->queue_declare('task_queue', false, true, false, false);
+        $function = $typeArray[$type]."type";
+        $this->$function($channel);
 
-        // 发送的消息内容
-        $data = "啦啦啦";
-        $msg = new AMQPMessage($data, array('delivery_mode' => 2)); // 设置消息为持久化
-
-        // 发布消息到队列
-        $channel->basic_publish($msg, '', 'task_queue');
-
-        echo " [x] Sent '$data'\n";
-
-        // 关闭连接
-
+        // 关闭信道和连接
         $channel->close();
         $connection->close();
+        return 'success';
+    }
+
+    /**
+     * 直连模式
+     * @param $channel
+     * @return void
+     */
+    private function directType(AMQPChannel $channel)
+    {
+        $messageBody = 'Hello World!['.time().']'; //消息内容
+        $exchangeName = 'test_exchange'; //交换机名字
+        $routingKey = 'test_routing_key'; //路由键
+
+        // 声明一个 exchange
+        $channel->exchange_declare($exchangeName,'direct',false,true,false);
+
+        // 创建一条消息
+        $message = new AMQPMessage($messageBody,['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
+
+        // 将消息发布到 exchange
+        $channel->basic_publish($message, $exchangeName, $routingKey);
+
+        echo " [x] Sent '" . $messageBody . "‘ to exchange ’test_exchange’ with routing key ’" . $routingKey . "‘\n";
+    }
+
+    /**
+     * 主题模式-发布&订阅
+     * @param $channel
+     * @return void
+     */
+    private function topicType(AMQPChannel $channel)
+    {
+        $exchangeName = 'topic_exchange'; //交换机名字
+
+        // 声明一个 topic 类型的 exchange
+        $channel->exchange_declare($exchangeName,'topic',false,true,false);
+
+        // 定义不同主题的消息
+        $messages = [
+            ['routing_key' => 'usa.news', 'body' => '美国新闻消息'],
+            ['routing_key' => 'europe.weather', 'body' => '欧洲天气预报'],
+            ['routing_key' => 'usa.weather', 'body' => '美国天气预报'],
+            ['routing_key' => 'chinese.weather', 'body' => '中国天气预报'],
+            ['routing_key' => 'asia.sports.basketball.news', 'body' => '亚洲篮球体育新闻'],
+            ['routing_key' => 'news', 'body' => '通用新闻'],
+            ['routing_key' => 'usa.news.sports', 'body' => '美国体育新闻'],
+        ];
+
+        // 发送消息
+        foreach($messages as $msg){
+            // 创建消息
+            $message = new AMQPMessage($msg['body'],[
+                'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT
+            ]);
+
+            // 发送
+            $channel->basic_publish($message, $exchangeName, $msg['routing_key']);
+            echo " [x] Sent '" . $msg['body'] . "' with routing key '" . $msg['routing_key'] . "'\n";
+        }
     }
 }
